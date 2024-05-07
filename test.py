b4488478a6f8c68fb2c7352637fe04d6ade0a6a0
agtoth1 <47246769+agtoth1@users.noreply.github.com>
2024-05-07 15:55:28 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+# add library imports
+import sys
+import re
+import zipfile
+from datetime import datetime
+import pickle
+
+# global variables:
+counts = {'fic':{}, 'news':{}, 'spok':{}, 'tvm':{}}
+verbs = {}
+
+def read_verbs(filename):
+  # read and load verbs:
+  with open(filename) as f: 
+    for line in f:
+      line = line.strip()
+      parts = line.split(" ")
+      verbs[parts[0]] = parts
+  # setup counts directories:
+  for v in verbs.keys():
+    counts['fic'][v]=[]
+    counts['news'][v]=[]
+    counts['spok'][v]=[]
+    counts['tvm'][v]=[]
+
+# more info:
+# https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
